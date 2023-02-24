3be5429d2d7bad79745e28b94d7081ef603b15ec
Dan Jørgensen <61663729+davijoe@users.noreply.github.com>
2023-02-24 21:45:30 +0100

    Uploaded initial dir setup with templates
    Fixed directory setup.

---
+++ src/main/java/com/justsomesoftware/wasteland/WastelandApplication.java
@@ -0,0 +1,13 @@
+package com.justsomesoftware.wasteland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WastelandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WastelandApplication.class, args);
+	}
+
+}
